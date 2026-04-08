263c429400918a84fad306b9bb8bc94c74419f72
Yama <pitbike216@gmail.com>
2026-04-08 19:00:01 +0200

    sm6375-common: overlay: Enable call recording

--- common.mk
+++ common.mk
@@ -227,6 +227,7 @@ $(call soong_config_set_bool,stagefright,target_disable_thumbnail_block_model,tr
 PRODUCT_PACKAGES += \
     MotoActions \
     MotoCommonOverlay \
+    LineageDialerOverlayBangkk \
     TurboPowerOverlay
 
 # Permissions
